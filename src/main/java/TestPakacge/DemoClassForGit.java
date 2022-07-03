af0d231aadcb4d6a2da404b914c3962b5db809e7
Billal19 <billal_yahiaoui@hotmail.com>
2022-07-03 17:22:35 -0400

    updated webdriver manager and added one demo class

---
+++ src/main/java/TestPakacge/DemoClassForGit.java
@@ -0,0 +1,10 @@
+package TestPakacge;
+
+public class DemoClassForGit {
+
+	public static void main(String[] args) {
+		System.out.println("this class is created for git practice only  , so we will be able to see it as an untracked file when we run the git status command");
+
+	}
+
+}
