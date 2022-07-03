964f6ff03d06ba763ae170bdf7cb865d6d277d4c
Billal19 <billal_yahiaoui@hotmail.com>
2022-07-03 17:34:41 -0400

    this the added new Sample class

---
+++ src/main/java/TestPakacge/SamplePageForGitCommits.java
@@ -0,0 +1,10 @@
+package TestPakacge;
+
+public class SamplePageForGitCommits {
+
+	public static void main(String[] args) {
+		System.out.println("this is class is created for Git Practices only to see how to commit the untracked files");
+
+	}
+
+}
